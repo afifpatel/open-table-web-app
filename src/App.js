abcce116925148f84df55e531e33c585b86eeeab
afifpatel89 <afifpatel89@gmail.com>
2020-05-02 15:25:21 -0400

    error fix in component set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
-import CryptoCurrencies from './containers/CryptoCurrencies';
+import CryptoCurrencies from './containers/CryptoCurrencies/CryptoCurrencies';
 
 const App = () => {
   return (
